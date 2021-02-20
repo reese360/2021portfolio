df97f3bc640d9b1c20238e6f5492b316614852df
reese <reese360@gmail.com>
2021-02-20 09:19:23 -0600

    various refactors + landing view animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 export class AppComponent {
 	@ViewChild('cursor') cursorBlock!: ElementRef;
 
+	@HostListener('contextmenu', ['$event']) onRightClick(e: any) {
+		e.preventDefault();
+	}
+
 	@HostListener('document:mousemove', ['$event']) onMouseMove(e: MouseEvent) {
 		this.cursorBlock.nativeElement.style.left = `${e.clientX - 5}px`;
 		this.cursorBlock.nativeElement.style.top = `${e.clientY - 5}px`;
--- src/app/views/landing-view/landing-view.component.ts
+++ src/app/views/landing-view/landing-view.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
 	selector: 'app-landing-view',
 	templateUrl: './landing-view.component.html',
 	styleUrls: ['./landing-view.component.scss'],
 })
-export class LandingViewComponent implements OnInit {
+export class LandingViewComponent implements AfterViewInit {
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngAfterViewInit(): void {}
+
+	@HostListener('document:mousedown', ['$event']) onMouseDown(e: MouseEvent): void {
+		console.log(true);
+		const elements: Element[] = Array.from(document.getElementsByClassName('text-enter'));
+		elements.forEach((el: Element) => {
+			el.classList.remove('text-enter');
+			el.classList.add('text-exit');
+		});
+	}
 }
