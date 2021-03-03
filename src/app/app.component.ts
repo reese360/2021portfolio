a4ca63e0ae3b1960f776d66b8fd3d67e84b05ef6
reese <reese360@gmail.com>
2021-03-03 16:24:38 -0600

    fixed app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
+import { WeatherService } from './services/weatherService.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
+	providers: [WeatherService],
 })
 export class AppComponent {
+	cursorHover: boolean = false;
+	constructor(private render: Renderer2) {}
 	@ViewChild('cursor') cursorBlock!: ElementRef;
 
 	@HostListener('contextmenu', ['$event']) onRightClick(e: any) {
@@ -13,7 +17,37 @@ export class AppComponent {
 	}
 
 	@HostListener('document:mousemove', ['$event']) onMouseMove(e: MouseEvent) {
-		this.cursorBlock.nativeElement.style.left = `${e.clientX - 5}px`;
-		this.cursorBlock.nativeElement.style.top = `${e.clientY - 5}px`;
+		// this.cursorBlock.nativeElement.style.left = `${e.clientX - 5}px`;
+		// this.cursorBlock.nativeElement.style.top = `${e.clientY - 5}px`;
+		// const query = document.querySelectorAll('.link-container:hover')[0];
+		// this.cursorHover = query ? true : false;
+		// if (this.cursorHover) {
+		// 	this.render.addClass(this.cursorBlock.nativeElement, 'cursor-hl');
+		// 	this.cursorBlock.nativeElement.style.left = `${e.clientX - 15}px`;
+		// 	this.cursorBlock.nativeElement.style.top = `${e.clientY - 15}px`;
+		// 	this.cursorHover = true;
+		// } else {
+		// 	this.render.removeClass(this.cursorBlock.nativeElement, 'cursor-hl');
+		// }
 	}
+
+	@HostListener('document:mousedown', ['$evetn']) onMouseDown(e: MouseEvent) {}
+
+	// @HostListener('mousewheel', ['$event']) onMousewheel(event: any) {
+	// 	// scroll up
+	// 	if (event.wheelDelta > 0) {
+	// 		const text = getComputedStyle(document.documentElement).getPropertyValue('--text-color');
+	// 		const background = getComputedStyle(document.documentElement).getPropertyValue('--main-background-color');
+	// 		document.documentElement.style.setProperty('--main-background-color', text);
+	// 		document.documentElement.style.setProperty('--text-color', background);
+	// 	}
+
+	// 	// scroll down
+	// 	if (event.wheelDelta < 0) {
+	// 		const text = getComputedStyle(document.documentElement).getPropertyValue('--text-color');
+	// 		const background = getComputedStyle(document.documentElement).getPropertyValue('--main-background-color');
+	// 		document.documentElement.style.setProperty('--main-background-color', text);
+	// 		document.documentElement.style.setProperty('--text-color', background);
+	// 	}
+	// }
 }
